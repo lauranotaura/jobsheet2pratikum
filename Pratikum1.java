7f04c54cc2c63d12dda5f50b937c07c3f2834ff5
lauranotaura <nifa.aura11@smk.belajar.id>
2026-02-23 14:42:40 +0700

    jb1

--- Pratikum1.java
+++ Pratikum1.java
@@ -1,40 +1,42 @@
 public class Pratikum1 {
-  
     String nama;
     String nim;
     String kelas;
-    String IPK;
+    double ipk;  
 
-    void tampilkanInformasi( ) {
-        System.out.println("Nama : " + nama);
-        System.out.println("NIM : " + nim);
+    void tampilkanInformasi() {
+        System.out.println("Nama  : " + nama);
+        System.out.println("NIM   : " + nim);
         System.out.println("Kelas : " + kelas);
-        System.out.println("IPK : " + IPK);
+        System.out.println("IPK   : " + ipk);
     }
+
     void ubahKelas(String kelasBaru) {
         kelas = kelasBaru;
+    }
 
-    }
-    void updateIPK(String IPKBaru) {
-        IPK = IPKBaru;
-    }
-    String nilaiKinerja() {
-        if (IPK.equals("A")) {
-            return "Sangat Baik";
-        } else if (IPK.equals("B")) {
-            return "Baik";
-        } else if (IPK.equals("C")) {
-            return "Cukup";
-        } else if (IPK.equals("D")) {
-            return "Kurang";
-        } else if (IPK.equals("E")) {
-            return "Sangat Kurang";
+    void updateIPK(double ipkBaru) { 
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
         } else {
-            return "Nilai IPK tidak valid";
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
         }
     }
 
+  
+    String nilaiKinerja() {
+        if (ipk >= 3.5) { 
+            return "Kinerja sangat baik"; 
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik"; 
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup"; 
+        } else {
+            return "Kinerja kurang"; 
+        }
+    }
 }
+   
 
   
    
